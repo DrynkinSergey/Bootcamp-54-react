bcd3d22a72a635ea130bab55bfaf3041c2df26e8
Sergey <sergsmile29@gmail.com>
2023-06-19 11:43:35 +0300

    createElement

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 
+const elem = React.createElement('a', {
+	href: '/',
+	title: 'Hello',
+	children: 'Link to:',
+})
+
+console.log(elem)
+// <a href=''> TEXT </a>
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+root.render(elem)
+
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App'
+
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(<App />)
