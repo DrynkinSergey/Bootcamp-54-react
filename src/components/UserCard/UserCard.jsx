9b88f004aed85b6eaf33ada70673492408b1a733
Sergey <sergsmile29@gmail.com>
2023-06-20 11:49:15 +0300

    clsx

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './UserCard.css'
-import s from './UserCard.module.css'
+import clsx from 'clsx'
+import s from './UserCard.module.scss'
 export const UserCard = ({ name, id, email, bio, isOpenToWork, skills }) => {
 	const classes = !isOpenToWork ? `${s.name} ${s.red}` : `${s.name} ${s.green}`
 
 	return (
 		<li className={s.card_item}>
-			<h1 className={classes}>{name}</h1>
+			<h1 className={clsx(s.name, isOpenToWork ? s.green : s.red)}>{name}</h1>
 			<h2>{email}</h2>
 			<p className={s.bio}>{bio}</p>
 			<ul>
