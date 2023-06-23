9894765495732dcd53c98f87e4f342db68008e6e
Sergey <sergsmile29@gmail.com>
2023-06-23 12:13:22 +0300

    split items for 2 lists

--- src/App.js
+++ src/App.js
@@ -7,17 +7,32 @@ import { ItemList } from './components/ItemList/ItemList'
 
 export class App extends Component {
 	state = {
-		data: [],
+		data: [
+			{ title: 'Harry Potter', id: 0, tag: 'books' },
+			{ title: 'Terminator', id: 1, tag: 'films' },
+			{ title: 'Mafia', id: 1, tag: 'films' },
+			{ title: 'Supernatural', id: 1, tag: 'films' },
+			{ title: 'Don kihot', id: 1, tag: 'books' },
+		],
 	}
+
+	getFilteredData = tag => {
+		const { data } = this.state
+		return data.filter(item => item.tag === tag)
+	}
+
 	render() {
 		const { data } = this.state
+
+		const books = this.getFilteredData('books')
+		const films = this.getFilteredData('films')
 		return (
 			<Wrapper>
 				<Header />
 				<AddItemForm />
 				<SelectItem />
-				<ItemList />
-				<ItemList />
+				<ItemList data={books} title='My books list' />
+				<ItemList data={films} title='My films list' />
 			</Wrapper>
 		)
 	}
--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export const ItemList = ({ data = [] }) => {
+export const ItemList = ({ data = [], title = 'My list' }) => {
 	console.log(data)
 	return (
 		<>
-			<h2>My books list</h2>
+			<h2>{title}</h2>
 			<ol>
 				{data.map(item => (
 					<li key={item.id}>{item.title}</li>
