10a3660b8c52ceb970e583b419cdc49dfe88fdd8
Sergey <sergsmile29@gmail.com>
2023-06-28 00:34:24 +0300

    change counter styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import { Counter } from './components/Counter/Counter'
 import { GitRepos } from './components/GitRepos/GitRepos'
 //https://api.github.com/search/repositories
 function App() {
 	return (
 		<>
-			<GitRepos />
+			<Counter />
 		</>
 	)
 }
