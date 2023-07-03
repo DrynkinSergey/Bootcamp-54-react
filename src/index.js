4655a8845dc02aca58393a459132314e41f6c54d
Sergey <sergsmile29@gmail.com>
2023-07-03 23:35:17 +0300

    init

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import ReactDOM from 'react-dom/client'
 import { App } from './App'
 import { Global } from './Styled/Global'
 import { BrowserRouter } from 'react-router-dom'
-import './index.css'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
