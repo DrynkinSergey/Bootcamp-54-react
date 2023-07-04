86afb49c9c5c91c7a448987141639ee40f55ad10
Sergey <sergsmile29@gmail.com>
2023-07-05 00:26:04 +0300

    clear

--- src/hooks/useAuth.jsx
+++
@@ -1,4 +0,0 @@
-import { useContext } from 'react'
-import { AuthContext } from '../context/ContextProvider'
-
-export const useAuth = () => useContext(AuthContext)
--- src/index.js
+++ src/index.js
@@ -4,16 +4,12 @@ import 'react-toastify/dist/ReactToastify.css'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
 import { Global } from './Styled/Global'
-import { BrowserRouter } from 'react-router-dom'
-import { ContextProvider } from './context/ContextProvider'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
-	<ContextProvider>
-		<BrowserRouter>
-			<App />
-			<Global />
-			<ToastContainer autoClose={1000} />
-		</BrowserRouter>
-	</ContextProvider>
+	<>
+		<App />
+		<Global />
+		<ToastContainer autoClose={1000} />
+	</>
 )
