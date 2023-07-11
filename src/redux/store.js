f287cddd13ce7754e58057deec7c8f5ac59f7252
Sergey <sergsmile29@gmail.com>
2023-07-12 00:33:05 +0300

    prepare

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,25 +1,14 @@
-import logger from 'redux-logger'
 import { todoReducer } from './todoList/slice'
 import { configureStore } from '@reduxjs/toolkit'
 import { filterReducer } from './todoList/filterSlice'
 import { themeReducer } from './themeSlice'
-
-const myFilterMiddleware = store => next => action => {
-	console.log(action)
-	if (action.payload?.title === 'Angular') {
-		//заміна значення в пейлоад на своє
-		action.payload.title = 'React'
-	}
-	next(action)
-}
-
+// https://goit-task-manager.herokuapp.com/
 export const store = configureStore({
 	reducer: {
 		todoList: todoReducer,
 		filter: filterReducer,
 		theme: themeReducer,
 	},
-	// middleware: getDefaultMiddleware =>
-	// 	getDefaultMiddleware().concat(myFilterMiddleware),
+
 	devTools: process.env.NODE_ENV !== 'production',
 })
