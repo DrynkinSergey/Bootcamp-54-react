5ab1355903bcfdb9d573c2caf81355f9b4c27654
Sergey <sergsmile29@gmail.com>
2023-07-14 00:05:44 +0300

    init qa

--- src/pages/Homepage.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export const Homepage = () => {
-	return <h1>Welcome to AUTH</h1>
-}
--- src/redux/auth/selectors.js
+++
@@ -1,9 +0,0 @@
-import { createSelector } from '@reduxjs/toolkit'
-
-export const selectLoggedIn = state => state.auth.isLoggedIn
-export const selectIsRefresh = state => state.auth.isRefreshing
-export const selectUserData = state => {
-	return state.auth.user
-}
-
-export const selectUser = createSelector([selectUserData], user => user)
